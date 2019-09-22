fae41b6488247c2c350d3843620095d924918e88
Руслан Гусейнов <guseinov.ruslan9@mail.ru>
2019-09-22 13:16:27 +0400

    Добавьте файлы проекта.

---
+++ epam/Array.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Arrays
+    {
+        int[] arr;
+        public Arrays(int size, int from, int before) //Заполняем массив случайными числами
+        {
+            arr = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+                arr[i] = rnd.Next(from, before);
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            foreach (int i in arr)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+        public void Index(int number) //Ищем индекс
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == number) Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ epam/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            Arrays arr = new Arrays(20, 1, 100);
+            Console.Write("Введите число:");
+            int number=int.Parse(Console.ReadLine());
+            Console.WriteLine("Введённое число найдено в следующих индексах массива:");
+            arr.Index(number);
+            Console.ReadLine(); 
+        }
+    }
+}
